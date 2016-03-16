add6ddef8c4ae28d0cd6a765344b35a4589e0b52
Mark Dane <dane@ohsu.edu>
2016-03-16 12:56:23 -0700

    Added listings of passed cell lines and barcodes Added new cell lines and normalized values to the output data

--- SelectWellData.R
+++ SelectWellData.R
@@ -2,7 +2,8 @@
 #Mark Dane 1/2015
 library(data.table)
 wDT <- fread("AtwatersWell_v1.1.txt")
-sDT <- wDT[wDT$CellLine %in% c("CAMA1","HCC1143","HCC1419","CAL51","SKBR3","JIMT1","Hs578T","SUM149PT","SUM159PT","MDAMB157","MDAMB231","ZR751","ZR75B","AU565","T47D","HCC38","HCC1806"),]
-sDT <- unique(sDT[,list(Barcode,Well,Plate,GeneSymbol,GeneAccession,GINumber,PoolCatalogNumber,CellLine,PlateID,Row,Column,MeanIntensityDAPI,MeanIntensityAlexa647,ElongationFactor,Perimeter,Area,WellCellCount,TotalIntensityDAPI,DNA2NProportion,DNA4NProportion,DNA2NProportionLogit,DNA4NProportionLogit,EduPositiveProportion,EduPositiveProportionLogit,G0G1Proportion,SProportion,G2Proportion,WellCellCountNorm,EduPositiveProportionLogitNorm)])
+sDT <- wDT[wDT$CellLine %in% c("CAMA1","HCC1143","HCC1419","CAL51","SKBR3","JIMT1","Hs578T","SUM149PT","SUM159PT","MDAMB157","MDAMB231","ZR751","ZR75B","AU565","T47D","HCC38","HCC1806","MDAMB134VI", "600MPE", "MDAMB361"),]
+
+sDT <- unique(sDT[,list(Barcode,Well,Plate,GeneSymbol,GeneAccession,GINumber,PoolCatalogNumber,CellLine,PlateID,Row,Column,MeanIntensityDAPI,MeanIntensityAlexa647,ElongationFactor,Perimeter,Area,WellCellCount,WellCellCountLoessNorm,TotalIntensityDAPI,DNA2NProportion,DNA4NProportion,DNA2NProportionLogit,DNA4NProportionLogit,EduPositiveProportion,EduPositiveProportionLogit,G0G1Proportion,SProportion,G2Proportion,WellCellCountNorm,EduPositiveProportionLogitNorm)])
 
 write.table(sDT,file="AtwatersSelectedWell.txt",quote = FALSE,sep = "\t",row.names = FALSE)
